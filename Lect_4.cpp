e1046788195c863edd50a64030b6863a192b2987
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-12 20:36:18 +0530

    Create Lect_4.cpp

---
+++ Lect_4.cpp
@@ -0,0 +1,25 @@
+//q1 basic print system
+//q2 Can be done with only
+//print,but loops makes it shorter.
+//q3 Needed input statement
+//(scanf) and some loop
+//construct
+
+//End Goals
+//playing with printf command
+//syntax and semantics of scanf command
+
+//Detour:
+//#computing  on computers.
+//#A bird's eye-view of the compuing machine.
+//How are numbers represented
+
+//How does the computer reprsent data?
+//To store: Numbers, text,graphics,and images,video,audio,
+//program instructions.
+//In some way,all information is diditized - broken down into pices and reprsented as numbers.
+//Example: Reprsenting text Digitally.
+//Every character is stored as a number, including spaces, digits,
+//and punctuation.
+//corresponding upper and lower cae are separate
+// characters.
