4e03876436b7dff1b8d7f7bfd7ba7b2ba81bafb8
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-12 23:49:11 +0530

    Create Lect_6.cpp

---
+++ Lect_6.cpp
@@ -0,0 +1,24 @@
+//Take two intgers x and y from user.
+// Exchange values in a and y. swap two intgers.
+// Print the values of x and y.
+// write a Cprogram to do the same
+#include<stdio.h>
+int main(){
+  int x,y;
+  printf("Enter x:");
+  scanf("%d",&x);
+  printf("Enter y:");
+  scanf("%d",&y);
+  
+  
+  printf("x=%d\n",x);
+  printf("x=%d\n",y);
+}
+// Basic operators: +,-,*,/
+// Modulus operator: %
+// x%y: remainder when x is divided by y.
+// assigment operator: = 
+//operator precedence:
+// first: parenthsized; inner-most to outer-most.
+// second: *,/,%: left to right.
+// third: +,-; left to right.
