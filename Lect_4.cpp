b2733a425ba9f22cd67abfd9019f562478573029
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-12 21:13:56 +0530

    Update Lect_4.cpp

--- Lect_4.cpp
+++ Lect_4.cpp
@@ -69,3 +69,12 @@ int main(){
 // o: octal
 // u: unsigned int
 // e: real decimal in exponent form
+
+#include<stdio.h>
+int main(){
+  float f1 = 23.4290;
+  printf ("The value of f1 : %f\n",f1);
+  printf ("The value of f1 : %3f\n",f1);
+  printf ("The value of f1 : %20f\n",f1);
+}
+  
