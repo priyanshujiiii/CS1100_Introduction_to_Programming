1aa09f3d9f3f23b9cb98be411638e5dfd5b358ae
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-12 14:09:21 +0530

    Create Lect_3.cpp

---
+++ Lect_3.cpp
@@ -0,0 +1,15 @@
+//My first C program
+
+#include<stdio.h>
+int main() {
+  printf("********\n");
+  printf("********\n");
+  printf("********\n");
+  printf("********\n");
+  return 0;
+}
+
+// stdio.h = standad library of input and output
+// main =  a function that every c program have
+// print.f = a useful library function to print several things in C
+
