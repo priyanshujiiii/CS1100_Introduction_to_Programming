86cfc63c95d53019dc6bfdf60ecc5f9b9c262c50
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-12 22:36:05 +0530

    Create Lect5.cpp

---
+++ Lect5.cpp
@@ -0,0 +1,19 @@
+//vARIABLE NAMING: RULES AND GOOD PRACTICES
+//Rules
+//Made up of letters,digits and underscore
+//cannot contain any other symbol.
+//are case sesitive
+// valid names: age, height, ji;
+// allowded but undesirable: alb2, i  j, AGe, AGE;
+// NOT ALLOWED: %AGE, 1WEIGHT, &WIDTH, CHAR, FOR
+//Declartrion
+int age;
+char mychar;
+float f1;
+//Memory is allocated for all of the three, but not initialized. how much memory is allocated? use sizeof
+// intilization and modification:
+int age = 10;
+int age; age = 30;
+// left and right hand of assignment.
+// printf(format-string,var1,var2,.......,varn)
+//
