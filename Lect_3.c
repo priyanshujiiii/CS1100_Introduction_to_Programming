f8918bab73599c112d42090573c4ad2b9906ace7
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-12 14:23:58 +0530

    Create Lect_3.c

---
+++ Lect_3.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main (void){
+  printf("hello world\n");
+  return 0;
+}
+
+// ls = this command help show what was in curent directory
+// gcc.main.c or gcc.Lect_3.c = this command help you complie a programm
+// a.out = binary executable command
+// ./a.out = its print the output ' here the doubt is all or one '
