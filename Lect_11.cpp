5fea2b9c9fb8f7da8603b0d16389bfa87a0e482b
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 17:17:07 +0530

    Update Lect_11.cpp

--- Lect_11.cpp
+++ Lect_11.cpp
@@ -1,1 +1,15 @@
-
+// Euclid's algorithm
+#include<stdio.h>
+main(){
+  int x,y;int temp;
+  scanf("%d %d,&x,&y");
+  if(x<y){
+    temp = x; x = y; y = temp;
+  }
+  // assume x >= y.
+  while(x%y! = 0){
+    x = x%y;
+    temp = x; x = y; y = temp;
+  }
+  printf("gcd of input numbers is %d \n",y);
+}
