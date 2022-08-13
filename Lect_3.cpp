3203c81d19c597a7b117106d031d759f98dc0b54
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 13:45:10 +0530

    Update Lect_3.cpp

--- Lect_3.cpp
+++ Lect_3.cpp
@@ -13,3 +13,15 @@ int main() {
 // main =  a function that every c program have
 // print.f = a useful library function to print several things in C
 
+
+#include <stdio.h>
+
+int main (void){
+  printf("hello world\n");
+  return 0;
+}
+
+// ls = this command help show what was in curent directory
+// gcc.main.c or gcc.Lect_3.c = this command help you complie a programm
+// a.out = binary executable command
+// ./a.out = its print the output ' here the doubt is all or one '
