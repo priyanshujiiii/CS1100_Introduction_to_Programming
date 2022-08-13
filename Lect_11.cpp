756e0dd913e6a61b634e21b0aa33bbad00789fa9
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 17:42:10 +0530

    Update Lect_11.cpp

--- Lect_11.cpp
+++ Lect_11.cpp
@@ -56,3 +56,17 @@ main(){
   }
   printf("gcd of input numbers is %d \n",y);
 }
+
+// Today Task: Computing a common mathematical function.
+// Compute positive square-root of an integer-- approximately
+
+// lets compute underroot 55
+// in fact we are interested in the value in the value at which the function f(x) = x^2 - 55 evaluates to zero !
+// start with some initial guess: say 1 ,. The value of f (!) is -ve!
+// pick another guess where value is positive say 25.
+// the function must be zero in between these two values.
+// two initial values xL = 1, xR = 25.
+// HOW DO WE PICK THE REFINED guess?
+// take mid-point
+// we now have 3 values. xL, xR, xM.
+// which are useful ? the two closest ones with opposite sign for f(x)
