ced3bb23eb4b090e548135d8158e164b955bdd17
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 22:34:54 +0530

    Update Lect_13.cpp

--- Lect_13.cpp
+++ Lect_13.cpp
@@ -1,1 +1,37 @@
 // nested loop
+// printing pattern
+// input
+#include<stdio.h>
+main(){
+  for(int i = 1;i<=4;i++){
+    for(int j =1; j<=8;j++){
+      print("*");
+    }
+    printf("\n");
+  }
+}
+// output
+//********
+//********
+//********
+//********
+
+
+// input
+#include<stdio.h>
+main(){
+  int k = 2;
+  for(inti=1;i<=4;i++){
+    for(intj =1;j<=k;j++){
+      printf("*");
+    }
+    printf("\n");
+    k = k+2;
+  }
+}
+//output:
+//**
+//****
+//******
+//********
+
