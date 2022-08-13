a75d846f2d063631d050df491df239b51988f63a
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 15:51:18 +0530

    Update Lect_10.cpp

--- Lect_10.cpp
+++ Lect_10.cpp
@@ -25,3 +25,22 @@ main(){
 
 // when control is at the scanf statement. we are scannin g the a-th input.
 // just before the statement a ++ w have computed min of first a elemnts given by user.         
+
+         
+// Finding min of positive integers: terminiated by a negative integer.
+
+#include<stdio.h>
+main(){
+  int n ; int currInt;
+  int min;
+  
+  scanf("%d",&currInt);
+  min = currInt;
+  while(currInt>=0){
+    scanf("%d",&currInt);
+    if (currInt<min){
+      min = currInt;
+    }
+  }
+  print("min = %d\n",min);
+}
