dff90b18b93139fdf6559cdd37ea483e36635e04
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 15:43:44 +0530

    Create Lect_10.cpp

---
+++ Lect_10.cpp
@@ -0,0 +1,27 @@
+// Finding min of n intgers
+#include<stdio.h>
+main(){
+  int n; int currInt;
+  int a; int min;
+  
+  scanf("%d",&n);
+  a = 1;
+  while(a<=n){
+    scanf("%d",&currint);
+    if (a==1){
+      min = currInt;
+    }
+    if (currInt < min){
+      min = currInt;
+    }
+    a++  
+  }
+  pritnf("min=%d\",min);
+}
+//points to remember
+// is the counter updated?
+// corner cases: a single input ?
+// min accurs as the first or last element.
+
+// when control is at the scanf statement. we are scannin g the a-th input.
+// just before the statement a ++ w have computed min of first a elemnts given by user.         
