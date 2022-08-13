5e34a6a261c54d16c23ebe0afdad68bda7acadc2
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 14:21:08 +0530

    Create Lect_9.cpp

---
+++ Lect_9.cpp
@@ -0,0 +1,26 @@
+// Our task today: One-Time mini calculator
+// Desired Behaviour
+// Prompts for operator, and two operands the result.
+// Repeats the above, unless asked to quit.
+repeat{
+  // Take operator , opoerands.
+  //Comput result and print
+} //untill (quit command is given)
+//repeat untill is not available in C!
+// available loop constructs: while, for,do-while
+
+
+// The while construct
+// syntax
+// while(expressions){statments;}
+// semantics
+// as long as expression is true, execute statements.
+// if expression is false, exit the loop.
+// value of expression must be changes by the body of the loop, otherwise we have an infinite loop.
+// expression can contain relitional logical or equailty operators.
+// Relational : '<=', '<','>','>='
+// equaility '==',!=
+// logical '&&' , '||'
+
+
+//
