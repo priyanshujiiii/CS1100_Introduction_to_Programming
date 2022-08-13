a44e1b078d9b5e7e3caff40a2441503b7d1cc400
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-13 12:24:09 +0530

    Update Lect_7.cpp

--- Lect_7.cpp
+++ Lect_7.cpp
@@ -87,4 +87,30 @@ int main(){
     printf("invalid character, enter one of w,a,b\n");
   }
 }
+//Relational operators and logical operators
+if(input =='w' || input == 'A'){
+  // do something
+}
+else{
+  // do something
+}
+// precedence of '==' is higher that '||'
+// the test expression is ((input == 'w') || (input == 'a'))
+// the parenthesis can therefore be avoided. add if it improves readability.
+// if (exp1 || exp2)
+// test is true if even one of exp is true .
+// evaluates exp1, if true , then does NOT evaluate exp2.
+// This is called short-circuiting.
+// How do it affect the program ?
+// if the exp2 has a side effect , or is a funaction call which changes something , the shor circuting , may lead to undesirable behaviour.
+// is useful too espcially when dealing with pointers.
   
+// Remark 5 : assignment and ==
+// if (input = 'w'){do}
+// else print error.
+// note the incorrect usage of assignment operator
+// what is the behaviour of the program ?
+// assignment operator retuen the value of the assignment.
+// How do it affect the program?
+// any non-zero value is trated as true and hence the program executes "do W"
+// No syntax error , no execution error these are nasy bugs!
