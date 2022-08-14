9e4ace6c8f13db8d27f86db2a71d7f008af6dd4c
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-14 16:50:28 +0530

    Create Lect_15.cpp

---
+++ Lect_15.cpp
@@ -0,0 +1,22 @@
+// One Dimensional arrays in c.
+// intger and character array.
+// manipulating arrrays.
+
+// Programming for real life problems
+//Here are some real life problems that we may want to solve using computers.
+//Given the marks obtained by students in asss clsasss , print as histogram.
+// Evaluate a polynomial a1x1 + a2x2 ......... anxn.
+//Encrypt and decrypt messages.
+
+// To store several elements of the same type.
+// store 100 intgers.
+// store 2000 characters.
+// store 500 floats.
+
+// Declaartion:
+// data - types array-name[array-size];
+// int marks[7];
+//char name [10]
+// float score score[1000];- defones 1000 vasssriable!
+// the value of marks [2] is 75.
+// new values can be assigned to elements marks[3] = 36;
