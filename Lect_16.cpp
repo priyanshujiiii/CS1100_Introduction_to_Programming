c1630ac34322249aba827c7788aefa68b155f232
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-15 17:13:09 +0530

    Update Lect_16.cpp

--- Lect_16.cpp
+++ Lect_16.cpp
@@ -1,1 +1,14 @@
+// Here are some real life problems that we may want to solve using computers.
+// Given the marks obtained by students un a class, print a histogram.
+// Evaluate a polynomial a0 + a1x +....
+// Encrypt and decrypt messages
+
+
+// Evaluating polynomial.
+// n is the degree of a polynomial.
+// user provides n coffecients.
+// user provides the value of x at which polynomial has to be evaluated.
+// evaluate the polynomial.
+
+// code:
 
