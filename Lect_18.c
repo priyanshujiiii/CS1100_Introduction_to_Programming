ec94544c5ced51020c14e1d77f55f975fe50a412
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-19 16:53:46 +0530

    Update Lect_18.c

--- Lect_18.c
+++ Lect_18.c
@@ -9,3 +9,8 @@
 // consider a "worst-case" input.
 // irrespective of whether the array is sorted or not , the method always needs n(n+)/2 comparisons.
 
+// 15 | 8 | 3 | 12 | 30 | 7 | 9 | 17 | 32 | 19
+// 15 | 8 | 3 | 12 | 30 | 7 | 9 | 17 | 32 | 19
+// 15 |12 | 8 | 3  | 30 | 7 | 9 | 17 | 32 | 19
+// ...........................................
+// 32 |30 |19 | 17 | 15 |12 | 9 |  8 | 7  | 3
