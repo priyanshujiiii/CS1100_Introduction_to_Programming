3a09856082e56c66b5bc6a45374c0a6df1042687
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-19 15:11:21 +0530

    Update Lect_17.c

--- Lect_17.c
+++ Lect_17.c
@@ -1,1 +1,23 @@
+// Seraching for an element in a sorted array.
+// 32 | 30 | 19 | 17 | 15 | 12 | 9 | 8 | 7 | 3
+// task :  Search for a key in a sorted array.
+// Binary search:
+               // Check the middle element . If found break.
+               // Else decide which part of the array is relevant and repeat.
+// can be done since array is sorted!
 
+int n , key; scanf("%d", &key);
+start = 0; end = n -1;
+while (________){
+  mid = ______ ;
+  if (A[mid]==key){
+    printf("Found at %d", mid);
+    break ;
+  }
+  if (A[mid]> key){
+    star = __________;
+  }
+  else{
+    end = __________;
+  }
+}
