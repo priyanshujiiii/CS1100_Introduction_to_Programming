a8731b9a3d9414173dd62d1dbf33c9fc07f9d1ae
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-19 16:48:38 +0530

    Update Lect_18.c

--- Lect_18.c
+++ Lect_18.c
@@ -1,1 +1,11 @@
 
+// Selection sort - number of comparisons
+
+// which input do we consider ?
+// Do number of comparison depend on the particular array values?
+// How does the method perfom when the array is nearly sorted ?
+
+
+// consider a "worst-case" input.
+// irrespective of whether the array is sorted or not , the method always needs n(n+)/2 comparisons.
+
