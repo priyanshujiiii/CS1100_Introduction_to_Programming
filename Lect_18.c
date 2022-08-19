f20daf58209875fa771768118dbf74539ad1bba3
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-19 17:48:38 +0530

    Update Lect_18.c

--- Lect_18.c
+++ Lect_18.c
@@ -30,3 +30,36 @@ for (i=1; i <=n-1; i++){
   
   // swap A[j] with A[j-1] till
 }
+
+// sol temp improve using while loop
+
+// Array index starts from 0.
+for (i=1; i <=n-1; i++){
+  j = i;
+  if (A[j]<= A[j-1])
+    continue;
+  // Now A[j] > A[j-1]
+  while (A[j] > A[j-1]){
+    // swap A[j] and A[j-1]
+    j = j-1
+  }
+}
+
+// Array index starts from 0.
+for (i=1; i <=n-1; i++){
+  j = i;
+  while (A[j] > A[j-1]&& j > b){  // note j > 0
+    // swap A[j] and A[j-1]       is it correct
+    j = j-1
+  }
+}
+
+// need to check (j>0) before checking A[j] > A[j-1]!
+// Note the use of short - circuting.
+
+
+// Sorting algorihms
+// Selection and insertion both require roughly n^2 comparisons on "worst-case" inputs.
+// Insertion sort performs significantly better on nearly sorted input data.
+// nealy sorted inputs occur in practice many times
+// Several other sorting algorithms (Quick sort , bubble-sort, Heap-sort) exist and many are implemented in libraries.
