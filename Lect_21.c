190dd4141628fca91562e637eb65c063b341185c
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-20 20:12:32 +0530

    Update Lect_21.c

--- Lect_21.c
+++ Lect_21.c
@@ -203,3 +203,24 @@ int main(){
 // >> use carefully - can have side-effects
 
 // swap function: 
+
+#include<stdio.h>
+void swap(int a , int b){
+  int temp = a;
+  a = b;
+  b = temp;
+  return ;
+}
+void main(){
+  int x = 20;
+  int y = 40;
+  swap (x,y);
+  printf("x=%d;y=%d\n",x,y);
+}
+// what is the input of the program ?
+// what is the output if we print a and binside the function swap ? (at the end of the function)
+// variables are passed are passed by value in c always?
+
+// Take - away : this is an incorrect swap program.
+// How do we wrie a correct swap program?
+
