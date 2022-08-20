2ed831b37a92a70649d526456bf9d6a089eb6e60
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-20 13:56:45 +0530

    Update Lect_19.c

--- Lect_19.c
+++ Lect_19.c
@@ -131,3 +131,35 @@ main(){
   // Roes /Column numbereed from 0 .
   // Storage row - major ordering elements of row 0 ,
   // elements of row 1, etc.
+
+// intialaizing  multi-dimensional arrays
+  #include<stdio.h>
+  main(){
+    int matrix1[3][4]={
+      1,2,3,4,
+      5,6,7,8,
+      9.10,11,12};
+    
+    int matrix2[3][4]={
+      1,2,3,4,
+      5,6,7,8,
+      9,10,11,12};
+  }
+  
+// Accessing Multi-dimensional arrays.
+  // what does the program print?
+  #include<stdio.h>
+  #define N1 3
+  #define N2 4
+  main(){
+    int matrix[N1][N2]={
+      1,2,3,4,
+      5,6,7,8,
+      9,10,11,12};
+    int i ;
+    for (i=0;i<N1;i++){
+      printf("%d\n",matrix[i][2]++);
+    }
+    for (i=0;i<N2, matrix[2][i]){
+      printf(%"d\t",matrix[2][i]);
+    }       
