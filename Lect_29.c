adee973b6a372a8f5b4f3a5a304eeaf0002ae605
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-22 01:32:20 +0530

    Update Lect_29.c

--- Lect_29.c
+++ Lect_29.c
@@ -1,1 +1,22 @@
+// Example#2 : sum and average of array elements
+// we wish to return sum and average of elements of an array.
+#include<stdio.h>
+int sumavg(int A,int n , float *avg){
+  int sum = 0;
+  for(int i =0;i<n;i++){
+    sum = sum + *(A+i);
+  }
+  *avg = sum/n;
+  return sum;
+}
+int main(){
+  int arr[5] = {1,2,3,4,5}
+  float mean;
+  int sum;
+  
+  sum = sumavg(arr,5,&mean);
+  printf("%d %f\n",sum,mean);
+}
+
+// okay lets fix the memory allocation issue
 
