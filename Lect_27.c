a18d96d29f7d45f3e2faaaf33e7e836a163c5426
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-21 22:05:37 +0530

    Update Lect_27.c

--- Lect_27.c
+++ Lect_27.c
@@ -1,1 +1,9 @@
+// Pointers - Recap
+//>> int count ; - names a memory cell called count.
+//>> Throughout the program the same memory cell gets accessed when we access count.
+//>> The address of count is called its l-value.
+//>> The value of count (its r-value) may change during the course of the program.
 
+//>> int *countptr; - names a memory cell called countptr.
+//>> Throughout the program the same memory cell gets accessed when we access countptr as i-value.
+//>> However different cells may get accessed when we access countptr as r-value
