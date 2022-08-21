66c1cef14bc071a68fa6aba9f69265693beb0a5c
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-21 12:38:37 +0530

    Update Lect_24.c

--- Lect_24.c
+++ Lect_24.c
@@ -1,1 +1,19 @@
+// Palindrome
+// Conceptual formulization
+// Assume inputs are a string str , start index , end index end.
+// Assume start <= end.
+//>>>> base case 1: if the str is of length 1 it is a palindrome.
+//>>>> base case 2: if str is of length 2 and str[start] == str [end],it is a palindrome.
+//>>>> Recursive step: if str[start] == str[end] and 
+//     str[start+1,end-1] is a palindrome, then str is a palindrome
+
+// Counting Digits of  non-negative intger
+//>>>> Base case: if n==0 return ______
+//>>>> Recursive step: return 1 + number of digits in n\10
+//>>>> if return value is 0, then answer is incorrect for n = 0
+//>>>> if return value is 1, then answer is incorrect for n = 123
+int digits(int n ){
+  if (n/10 == 0) return 1;
+  else return 1 + digits(n/10);
+}
 
