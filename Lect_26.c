98073d4685982cf05966274d34b34c6b11de84d5
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-21 21:01:36 +0530

    Update Lect_26.c

--- Lect_26.c
+++ Lect_26.c
@@ -1,1 +1,16 @@
+// What is pointer ?
+// intx; : x is the name of a variable that holds data which is of 
+// type intger , Similarly float; double d; and char c;
+
+
+// Pointer variable is a variable whose value is an address.
+// Syntax: data-type:
+// data-type * var-name;
+
+// Example:
+// int *ptr; // ptr is declared tp be a variable that can hold the address of an integer variable.
+// For instance , it can hold the address 65524 which will be the address of the variable i.
+//  |  i   |   -- location name
+//  |  5   |   -- value
+//  |65524 |   -- address of location
 
